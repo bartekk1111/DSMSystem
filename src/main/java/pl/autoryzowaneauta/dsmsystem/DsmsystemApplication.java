274cb8e40409a9cee51883b79b5c4565a11066dc
Bartosz Kęskrawiec <bartosz_keskrawiec@wp.pl>
2025-11-24 14:09:05 +0100

    Create Java Spring project

---
+++ src/main/java/pl/autoryzowaneauta/dsmsystem/DsmsystemApplication.java
@@ -0,0 +1,13 @@
+package pl.autoryzowaneauta.dsmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmsystemApplication.class, args);
+	}
+
+}
